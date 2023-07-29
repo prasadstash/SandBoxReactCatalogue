0d9673c410966161ed65577ad6f1a0a8754fa00d
Prasad <104647893+prasadstash@users.noreply.github.com>
2023-07-29 18:20:39 +0000

    products code

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
 
 export default function Products() {
+
+  const [data, setData]= useState([])
+
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+    .then(res => setData(res))
+    .catch(err => console.log(err));
+
+  },[])
+
   return (
-    <div className="products">
-      <h1>Products</h1>
+    <div className="table_container">
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>albumId</th>
+            <th>url</th>
+            <th>thumbnailUrl</th>
+            <th>title</th>
+          </tr>
+        </thead>
+        <tbody>
+            {
+              data.map((user, index) => 
+              <tr key={index}>
+                <td>userid</td>
+                <td>user.albumId</td>
+                <td>user.url</td>
+                <td>user.thumbnailUrl</td>
+                <td>user.title</td>
+              </tr>
+              ) 
+            }
+        </tbody>
+      </table>
     </div>
   );
 }
