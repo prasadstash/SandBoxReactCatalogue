5e059499cbb7462bffdac27aaa4075a38c36d4c2
prasadstash <104647893+prasadstash@users.noreply.github.com>
2023-07-29 19:49:36 +0530

    Initial commit

---
+++ src/components/SlidebarData.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as RiIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text"
+  },
+  {
+    title: "Reports",
+    // path: "/reports",
+    icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+    iconClosed: <RiIcons.RiArrowDownSFill/>,
+    iconOpen: <RiIcons.RiArrowDownSFill/>,
+    subNav: [
+      {
+        title: "Home",
+        path: "/",
+        icon: <AiIcons.AiFillHome />
+      },
+      {
+        title: "Home",
+        path: "/",
+        icon: <AiIcons.AiFillHome />
+      }
+    ]
+  },
+  {
+    title: "Products",
+    path: "/products",
+    icon: <FaIcons.FaCartPlus />,
+    cName: "nav-text"
+  },
+  {
+    title: "Team",
+    path: "/team",
+    icon: <IoIcons.IoMdPeople />,
+    cName: "nav-text"
+  },
+  {
+    title: "Messages",
+    path: "/",
+    icon: <FaIcons.FaEnvelopeOpenText />,
+    cName: "nav-text"
+  },
+  {
+    title: "Support",
+    path: "/support",
+    icon: <IoIcons.IoMdHelpCircle />,
+    cName: "nav-text"
+  }
+];
